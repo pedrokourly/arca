f128e1a6a416eabb6694a286ded1be7fdcb405f0
KyKirma <pedrohenriquekourly@hotmail.com>
2025-08-13 16:04:01 -0300

    feat: refactor backend structure by removing unused files and adding Prisma module and service

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: process.env.NODE_ENV
         ? `.env.${process.env.NODE_ENV}`
         : '.env',
-    })
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
