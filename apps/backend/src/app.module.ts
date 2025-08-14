229d39c349a8571d7938c45fd31d7419b3478237
Pedro Kourly <74114821+pedrokourly@users.noreply.github.com>
2025-08-13 22:37:13 -0300

    feat: implement authentication module with JWT and local strategies (#32)
    - Added AuthModule, AuthService, and strategies for JWT and local authentication.
    - Created AppController for handling login and profile routes.
    - Introduced UsersModule and UsersService for user management.
    - Updated package.json to include necessary dependencies for authentication.
    - Removed old controller and service files to streamline the codebase.

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: process.env.NODE_ENV
-        ? `.env.${process.env.NODE_ENV}`
-        : '.env',
+        isGlobal: true,
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
