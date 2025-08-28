84cb5b28822a5f1511805d61aff4086c86fcb82f
KyKirma <pedrohenriquekourly@hotmail.com>
2025-08-27 22:16:43 -0300

    feat: implement waitlist form with success alert and ID display

---
+++ apps/frontend/app/lista-espera/consulta/page.tsx
@@ -0,0 +1,7 @@
+export default function CreatePacientForm() {
+  return (
+    <div className="flex justify-center items-center min-h-screen p-8 pb-20 gap-12 sm:p-20 font-sans">
+      
+    </div>
+  );
+}
