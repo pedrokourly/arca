ee14a0d81c3bb9bccfa784dbfaed5ea2c5577e2c
Gustavo Corrêa <ecfgustavo@gmail.com>
2026-04-22 20:09:01 -0300

    feat: add initial environment configuration and layout components
    - Created .env.example for environment variables
    - Implemented Footer component with basic structure
    - Implemented Header component with basic structure

--- apps/frontend/components/agenda/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as AgendaGeral } from "./Agenda";
-export { default as Evento } from "./Evento";
-export { default as EventoDetalhes } from "./EventoDetalhes";
-
-// Exportar tipos também
-export type { EventoAgenda } from "./Agenda";
---
+++ apps/frontend/components/layout/footer.tsx
@@ -0,0 +1,15 @@
+// Directives
+'use client';
+
+// Imports Components
+// ...
+
+const Footer = () => {
+    return (
+        <footer>
+            <h1>Footer!!!</h1>
+        </footer>
+    )
+};
+
+export default Footer;
