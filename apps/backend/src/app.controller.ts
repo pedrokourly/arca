20758104c542983839c2b527610ece1945ac9a0e
KyKirma <pedrohenriquekourly@hotmail.com>
2026-04-27 21:08:20 -0300

    docs: add swagger annotations to app healthcheck

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
+    @ApiOperation({ summary: 'Healthcheck' })
+    @ApiResponse({ status: 200, description: 'API is healthy' })
     @Get()
     healthCheck() {
         return {
